6aa2f27199587842d8adfa135d9d53f7e559d815
subodh mishra <subodh166148@gmail.com>
2019-12-17 20:04:25 +0530

    changes on17-12-19

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React, { Component } from 'react';
-
-import Layout from './components/Layout/Layout';
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import React, { Component } from "react";
+import Layout from "./components/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
 class App extends Component {
-  render () {
+  render() {
     return (
       <div>
         <Layout>
           <BurgerBuilder />
-        </Layout>
+        </Layout>{" "}
       </div>
     );
   }
